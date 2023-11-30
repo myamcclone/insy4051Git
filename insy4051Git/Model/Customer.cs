6da9336f4338dde831ef60a5eba02183967049c9
myamcclone <mya.mcclone@marquette.edu>
2023-11-29 19:56:24 -0600

    Created the Model folder and the Customer class.

---
+++ insy4051Git/Model/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace insy4051Git.Model
+{
+    public class Customer
+    {
+    }
+}
